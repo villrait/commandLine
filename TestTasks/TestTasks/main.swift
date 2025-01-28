e2a25e2b053b258f23d154e486b28b1fa743bcc0
villrait <166745091+villrait@users.noreply.github.com>
2025-01-29 01:00:54 +0500

    Add solution for Double variables tas

--- TestTasks/TestTasks/main.swift
+++ TestTasks/TestTasks/main.swift
@@ -1,11 +1,26 @@
-//
-//  main.swift
-//  TestTasks
-//
-//  Created by м on 28.01.2025.
-//
-
 import Foundation
 
-print("Hello, World!")
+//5 Объявить две опциональные переменные типа Double. Одной из них задать первоначальное значение. Написать функцию, которая принимает на вход опциональную переменную типа Double. Функция должна безопасно извлечь значение из входящей переменной. Если значение удалось получить - необходимо вывести его в консоль, если значение у переменной отсутствует вывести в консоль фразу "Variable can't be unwrapped". Вызвать функцию дважды с двумя ранее созданными переменными.
+//
 
+protocol Testable {
+    func runTest()
+}
+
+class TestTask: Testable {
+    private func show(_ value: Double?) { // имя функции show потому что при слове print - код ломается. В ифах ошибки возникают
+        if let val = value {
+            print(val)
+        } else {
+            print("Variable can't be unwrapped")
+        }
+    }
+    
+    func runTest() {
+        var doubleOptional: Double? = 2.2
+        var doubleOptional2: Double?
+        
+        show(doubleOptional)
+        show(doubleOptional2)
+    }
+}
