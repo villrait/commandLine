d232055a0d7b34cc908a2966404802880c1ed1b8
villrait <166745091+villrait@users.noreply.github.com>
2025-02-02 23:04:54 +0500

    add RectangleTask commit

--- TestTasks/TestTasks/main.swift
+++ TestTasks/TestTasks/main.swift
@@ -163,6 +163,38 @@ class LocationTask: Testable {
     }
 }
 
+//11. Создать класс Rectangle с двумя неопциональными свойствами: ширина и длина. Реализовать в этом классе метод вычисляющий и выводящий в консоль периметр прямоугольника. Создать экземпляр класса и вызвать у него этот метод.
+
+class RectangleTask: Testable {
+    let width: Int
+    let lenght: Int
+    
+    init(width: Int, lenght: Int) {
+        self.width = width
+        self.lenght = lenght
+    }
+    
+    private func calcPerimeter() -> Int {
+        (width + lenght) * 2
+    }
+    
+    func runTest() {
+        let rectangle = calcPerimeter()
+        print("Perimeter = \(rectangle)")
+    }
+    
+}
+
+let rectangle1 = RectangleTask(width: 4, lenght: 10)
+let rectangle2 = RectangleTask(width: 3, lenght: 6)
+
+let tasks: [Testable] = [rectangle1, rectangle2]
+
+for task in tasks {
+    task.runTest()
+}
+
+
 let tasks: [Testable] = [TestTask(), Fibonachi(), Bubble(), IncrementNumberTask(), ClosureTask(), LocationTask()]
 
 for task in tasks {
